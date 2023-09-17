d59bd38b6c4428da707d842916b6dfdecd875f3b
Boris Kucherov <boris.ku4erov@gmail.com>
2023-09-17 17:41:51 +0300

    add m2m teachers and students relation

--- school/admin.py
+++ school/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class TeachersInline(admin.TabularInline):
+    extra = 1
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     pass
--- school/views.py
+++ school/views.py
@@ -1,12 +1,13 @@
-from django.views.generic import ListView
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    students = Student.objects.all()
+    context = {'object_list': students}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
